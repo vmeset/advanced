3806253e6e84438767a8cc1fe40b9e430947944f
vmeset <luzin.itrum@gmail.com>
2023-08-14 16:54:06 +0300

    navbar widget and applink ui component developed

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,32 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./AppLink.module.scss";
+import { Link, LinkProps } from "react-router-dom";
+
+export enum AppLinkTheme {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink = (props: AppLinkProps): JSX.Element => {
+  const {
+    to,
+    className,
+    children,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
+  return (
+    <Link
+      to={to}
+      className={classNames(cls.applink, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};

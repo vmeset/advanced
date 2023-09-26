6132328358a3ed1ccd971a717a99d1a32e2b6155
vmeset <vmeset@gmail.com>
2023-09-26 08:24:13 +0300

    pageLoader developed

---
+++ src/pages/notfound/index.ts
@@ -0,0 +1,3 @@
+import { NotFoundPage } from './ui/NotFound'
+
+export { NotFoundPage }
---
+++ src/pages/notfound/ui/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+export const NotFoundPage = () => {
+  const { t } = useTranslation('not_found')
+  return (
+    <div>{t('Страница не найдена')}</div>
+  )
+}

ecaa3ab06962a1371276233ec17332300ae87ce4
vmeset <vmeset@gmail.com>
2023-10-10 15:26:08 +0300

    config fixed

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -8,26 +8,31 @@ export default ({ config }: { config: webpack.Configuration }) => {
     build: '',
     html: '',
     entry: '',
-    src: path.resolve(__dirname, '..', '..', 'src'),
+    src: path.resolve(__dirname, '../../src'),
   }
-  config?.resolve?.modules?.push(paths.src)
-  config?.resolve?.extensions?.push('.ts', '.tsx')
+  config.resolve?.modules?.push(paths.src)
+  config.resolve?.extensions?.push('.ts', '.tsx')
 
-  config.module.rules = config?.module?.rules?.map((rule: RuleSetRule) => {
-    // eslint-disable-next-line @typescript-eslint/prefer-includes
-    if (/svg/.test(rule.test as string)) {
-      return { ...rule, exclude: /\.svg$/i }
-    }
+  if (config.module?.rules) {
+    config.module.rules = config.module?.rules?.map(
+      (rule: RuleSetRule | null | undefined | false | 0 | '' | '...') => {
+        // eslint-disable-next-line @typescript-eslint/prefer-includes
+        if (rule && rule !== '...' && /svg/.test(rule.test as string)) {
+          // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+          return { ...rule, exclude: /\.svg$/i }
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+        return rule
+      }
+    )
+  }
 
-    return rule
-  })
-
-  config.module.rules.push({
+  config.module?.rules?.push({
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   })
 
-  config?.module?.rules?.push(buildCssLoader(true))
+  config.module?.rules?.push(buildCssLoader(true))
 
   return config
 }

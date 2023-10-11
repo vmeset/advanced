6782b83e9758176ae27336f5ff91e132f24f2264
vmeset <vmeset@gmail.com>
2023-10-11 10:50:30 +0300

    pageloader SBed

---
+++ src/widgets/PageLoader/ui/PageLoader.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Theme } from 'app/providers/ThemeProvider'
+import { ThemeDecorator } from '../../../shared/config/storybook/themeDecorator/ThemeDecorator'
+import { PageLoader } from './PageLoader'
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+  title: 'widget/PageLoader',
+  component: PageLoader,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof PageLoader>
+
+const Template: ComponentStory<typeof PageLoader> = (args) => (
+  <PageLoader {...args} />
+)
+
+export const LightPageLoader = Template.bind({})
+LightPageLoader.args = {}
+
+export const DarkPageLoader = Template.bind({})
+DarkPageLoader.args = {}
+DarkPageLoader.decorators = [ThemeDecorator(Theme.DARK)]

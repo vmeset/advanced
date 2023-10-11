3d3c940b79a844a20cd5e593a8aa1ab7b357b345
vmeset <vmeset@gmail.com>
2023-10-11 14:08:07 +0300

    pages SBed

--- src/pages/about/ui/About.stories.tsx
+++ src/pages/about/ui/About.stories.tsx
@@ -1,24 +1,22 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { Theme } from 'app/providers/ThemeProvider'
-import { ThemeSwitcher } from './ThemeSwitcher'
 import { ThemeDecorator } from '../../../shared/config/storybook/themeDecorator/ThemeDecorator'
+import About from './About'
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default {
-  title: 'widget/ThemeSwitcher',
-  component: ThemeSwitcher,
+  title: 'pages/About',
+  component: About,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
   args: {
     to: '/',
   },
-} as ComponentMeta<typeof ThemeSwitcher>
+} as ComponentMeta<typeof About>
 
-const Template: ComponentStory<typeof ThemeSwitcher> = (args) => (
-  <ThemeSwitcher {...args} />
-)
+const Template: ComponentStory<typeof About> = (args) => <About {...args} />
 
 export const Light = Template.bind({})
 Light.args = {}
--- src/pages/main/ui/Main.stories.tsx
+++ src/pages/main/ui/Main.stories.tsx
@@ -1,24 +1,22 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { Theme } from 'app/providers/ThemeProvider'
-import { ThemeSwitcher } from './ThemeSwitcher'
 import { ThemeDecorator } from '../../../shared/config/storybook/themeDecorator/ThemeDecorator'
+import Main from './Main'
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default {
-  title: 'widget/ThemeSwitcher',
-  component: ThemeSwitcher,
+  title: 'pages/Main',
+  component: Main,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
   args: {
     to: '/',
   },
-} as ComponentMeta<typeof ThemeSwitcher>
+} as ComponentMeta<typeof Main>
 
-const Template: ComponentStory<typeof ThemeSwitcher> = (args) => (
-  <ThemeSwitcher {...args} />
-)
+const Template: ComponentStory<typeof Main> = (args) => <Main {...args} />
 
 export const Light = Template.bind({})
 Light.args = {}

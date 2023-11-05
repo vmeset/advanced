50dc4125465df012f36bbea39123255b4eef1de8
vmeset <vmeset@gmail.com>
2023-11-05 10:05:53 +0300

    portal modal for navbar; new screenshots for loki

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -3,8 +3,17 @@ import { Theme, ThemeContext } from '../lib/ThemeContext'
 
 const defaultTheme = (localStorage.getItem('theme') as Theme) || Theme.LIGHT
 
-const ThemeProvider: FC = ({ children }) => {
-  const [theme, setTheme] = useState<Theme>(defaultTheme)
+interface ThemeProviderProps {
+  initialTheme?: Theme
+}
+
+const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+  const {
+    children,
+    initialTheme
+  } = props
+
+  const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme)
 
   const defaultProps = useMemo(
     () => ({ theme, setTheme }),
--- src/shared/config/storybook/themeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/themeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,12 @@
 import { Story } from '@storybook/react'
-import { Theme } from 'app/providers/ThemeProvider'
-import React from 'react'
+import { Theme, ThemeProvider } from 'app/providers/ThemeProvider'
 
+// eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) =>
   (
-    <div className={`app ${theme}`}>
-      <StoryComponent />
-    </div>
+    <ThemeProvider initialTheme={theme}>
+      <div className={`app ${theme}`}>
+        <StoryComponent />
+      </div>
+    </ThemeProvider>
   )

c09bfeb0e27438eafa1517d02abf0b80b7952180
vmeset <vmeset@gmail.com>
2023-12-20 15:45:11 +0300

    Profile entitie created, initial slice and types

---
+++ src/shared/const/common.ts
@@ -0,0 +1,12 @@
+export enum Currencies {
+  RUB = 'RUB',
+  USD = 'USD',
+  CNY = 'CNY',
+}
+
+export enum Countries {
+  RUSSIA = 'Russia',
+  Belarus = 'Belarus',
+  Ukraine = 'Ukraine',
+  Kazakhstan = 'Kazakhstan',
+}

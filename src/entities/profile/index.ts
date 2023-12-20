807af67e644f6d625fcbdbf62df80d18a0c6b4e6
vmeset <vmeset@gmail.com>
2023-12-20 16:00:51 +0300

    async profileReducer

--- src/entities/profile/index.ts
+++ src/entities/profile/index.ts
@@ -1,1 +1,3 @@
+export { profileActions, profileReducer } from './model/slice/profileSlice'
+
 export type { IProfile, ProfileSchema } from './model/types'
--- src/pages/profile/ui/Profile.tsx
+++ src/pages/profile/ui/Profile.tsx
@@ -2,17 +2,28 @@ import { useTranslation } from 'react-i18next'
 import cls from './Profile.module.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { memo } from 'react'
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from 'shared/lib/components/DynamicModuleLoader'
+import { profileReducer } from 'entities/profile'
 
 interface ProfileProps {
   className?: string
 }
 
+const initialReducers: ReducersList = {
+  profile: profileReducer,
+}
+
 const Profile = memo(function Profile({ className }: ProfileProps) {
   const { t } = useTranslation('profile')
   return (
-    <div className={classNames(cls.Profile, {}, [className])}>
-      {t('Страница профиля')}
-    </div>
+    <DynamicModuleLoader reducers={initialReducers} removeAfterUnmount>
+      <div className={classNames(cls.Profile, {}, [className])}>
+        {t('Страница профиля')}
+      </div>
+    </DynamicModuleLoader>
   )
 })
 
